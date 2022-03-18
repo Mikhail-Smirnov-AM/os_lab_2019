f9d23cf71fb025e195f81f81f489d2162a81d6ef
Mikhail-Smirnov <>
2022-03-18 14:46:31 +0000

    Добавлены решения заданий лабораторной работы №3

--- lab3/src/parallel_min_max.c
+++ lab3/src/parallel_min_max.c
@@ -15,13 +15,15 @@
 #include "find_min_max.h"
 #include "utils.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
   int seed = -1;
   int array_size = -1;
   int pnum = -1;
   bool with_files = false;
 
-  while (true) {
+  while (true) 
+  {
     int current_optind = optind ? optind : 1;
 
     static struct option options[] = {{"seed", required_argument, 0, 0},
@@ -35,23 +37,22 @@ int main(int argc, char **argv) {
 
     if (c == -1) break;
 
-    switch (c) {
+    switch (c) 
+    {
       case 0:
-        switch (option_index) {
+        switch (option_index) 
+        {
           case 0:
             seed = atoi(optarg);
-            // your code here
-            // error handling
+            if (seed <= 0) seed = -1;
             break;
           case 1:
             array_size = atoi(optarg);
-            // your code here
-            // error handling
+            if (array_size <= 0) array_size = -1;
             break;
           case 2:
             pnum = atoi(optarg);
-            // your code here
-            // error handling
+            if (pnum <= 0) pnum = -1;
             break;
           case 3:
             with_files = true;
@@ -66,6 +67,7 @@ int main(int argc, char **argv) {
         break;
 
       case '?':
+        printf("Some error has been occurred. getopt_long returned ?\n");
         break;
 
       default:
@@ -73,12 +75,14 @@ int main(int argc, char **argv) {
     }
   }
 
-  if (optind < argc) {
+  if (optind < argc) 
+  {
     printf("Has at least one no option argument\n");
     return 1;
   }
 
-  if (seed == -1 || array_size == -1 || pnum == -1) {
+  if (seed == -1 || array_size == -1 || pnum == -1) 
+  {
     printf("Usage: %s --seed \"num\" --array_size \"num\" --pnum \"num\" \n",
            argv[0]);
     return 1;
@@ -87,52 +91,87 @@ int main(int argc, char **argv) {
   int *array = malloc(sizeof(int) * array_size);
   GenerateArray(array, array_size, seed);
   int active_child_processes = 0;
+  
+  int pipefd[2];
+  if (pipe(pipefd) == -1) 
+  {
+    printf("Pipe failed!\n");
+    return 1;
+  }
 
+  FILE* file = fopen("parallel_synch.txt","w");
+  if (file == NULL)
+  {
+    printf("fopen error!\n");
+    return 1;
+  }
+  
   struct timeval start_time;
   gettimeofday(&start_time, NULL);
 
-  for (int i = 0; i < pnum; i++) {
+  for (int i = 0; i < pnum; i++) 
+  {
     pid_t child_pid = fork();
-    if (child_pid >= 0) {
+    if (child_pid >= 0) 
+    {
       // successful fork
       active_child_processes += 1;
-      if (child_pid == 0) {
+      if (child_pid == 0) 
+      {
         // child process
+        struct MinMax min_max = GetMinMax(array,i*array_size/pnum,(i+1)*array_size/pnum);
 
-        // parallel somehow
-
-        if (with_files) {
-          // use files here
-        } else {
-          // use pipe here
+        if (with_files) 
+        {
+          fprintf(file, "%d\n",min_max.min);
+          fprintf(file, "%d\n",min_max.max);
+        } 
+        else 
+        {
+          write(pipefd[1],&min_max,8);
         }
-        return 0;
+        exit(0);
       }
-
-    } else {
+    } 
+    else 
+    {
       printf("Fork failed!\n");
       return 1;
     }
   }
 
-  while (active_child_processes > 0) {
-    // your code here
-
+  while (active_child_processes > 0) 
+  {
+    wait(0);
     active_child_processes -= 1;
   }
-
+  
+  file = fopen("parallel_synch.txt","r");
+  if (file == NULL)
+  {
+    printf("fopen error!\n");
+    return 1;
+  }
+    
   struct MinMax min_max;
   min_max.min = INT_MAX;
   min_max.max = INT_MIN;
 
-  for (int i = 0; i < pnum; i++) {
+  for (int i = 0; i < pnum; i++) 
+  {
     int min = INT_MAX;
     int max = INT_MIN;
-
-    if (with_files) {
-      // read from files
-    } else {
-      // read from pipes
+    char c;
+    
+    if (with_files) 
+    {
+      fscanf(file,"%d",&min);
+      fscanf(file,"%d",&max);
+    } 
+    else 
+    {
+      read(pipefd[0],&min,4);
+      read(pipefd[0],&max,4);
     }
 
     if (min < min_max.min) min_max.min = min;
@@ -145,6 +184,9 @@ int main(int argc, char **argv) {
   double elapsed_time = (finish_time.tv_sec - start_time.tv_sec) * 1000.0;
   elapsed_time += (finish_time.tv_usec - start_time.tv_usec) / 1000.0;
 
+  fclose(file);
+  close(pipefd[0]);
+  close(pipefd[1]);
   free(array);
 
   printf("Min: %d\n", min_max.min);
